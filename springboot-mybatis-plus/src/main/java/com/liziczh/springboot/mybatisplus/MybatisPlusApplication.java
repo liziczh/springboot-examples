d2105d814f5b6ece8022c20114928d01814da738
zhehao.chen <zhehao.chen@ikang.com>
2020-09-15 18:55:23 +0800

    mybatisplus

--- springboot-mybatis-plus/src/main/java/com/liziczh/springboot/mybatisplus/MybatisPlusApplication.java
+++ springboot-mybatis-plus/src/main/java/com/liziczh/springboot/mybatisplus/MybatisPlusApplication.java
@@ -1,9 +1,15 @@
 package com.liziczh.springboot.mybatisplus;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = { "com.liziczh.**.mybatisplus" })
+@MapperScan("com.liziczh.**.mapper")
+@EntityScan(basePackages = "com.liziczh.**.entity")
+@EnableCaching
 public class MybatisPlusApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MybatisPlusApplication.class, args);
