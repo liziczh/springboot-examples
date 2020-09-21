1c24a1180186dc19b687f1510275974c656745c8
liziczh <liziczh@foxmail.com>
2020-09-21 18:34:06 +0800

    mp

--- springboot-mybatis-plus/src/main/java/com/liziczh/springboot/mybatisplus/service/impl/DemoServiceImpl.java
+++ springboot-mybatis-plus/src/main/java/com/liziczh/springboot/mybatisplus/service/impl/DemoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.liziczh.springboot.mybatisplus.service.impl;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,9 @@ import org.springframework.stereotype.Service;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.liziczh.base.common.condition.PageCondition;
+import com.liziczh.base.common.condition.SortCondition;
 import com.liziczh.springboot.mybatisplus.condition.DemoCondition;
+import com.liziczh.springboot.mybatisplus.constant.Constants;
 import com.liziczh.springboot.mybatisplus.entity.TDemo;
 import com.liziczh.springboot.mybatisplus.mapper.TDemoMapper;
 import com.liziczh.springboot.mybatisplus.service.DemoService;
@@ -28,15 +31,18 @@ public class DemoServiceImpl implements DemoService {
 	@Override
 	public Page<TDemo> selectPage(DemoCondition condition) {
 		PageCondition pageCondition = condition.getPageCondition();
-		TDemo demoCondition = new TDemo();
-		demoCondition.setName("栗子");
-		return demoMapper.selectPage(new Page<>(pageCondition.getPageNum(), pageCondition.getPageSize()), new QueryWrapper<>(demoCondition));
+		List<SortCondition> sortConditionList = condition.getSortConditionList();
+		QueryWrapper<TDemo> queryWrapper = new QueryWrapper<>();
+		queryWrapper.lambda().like(TDemo::getName, condition.getName()).lt(TDemo::getCreateTime, new Date()).eq(TDemo::getValid, Constants.COMMON_STATUS.VALID.getCode());
+		return demoMapper.selectPage(new Page<>(pageCondition.getPageNum(), pageCondition.getPageSize()), queryWrapper);
 	}
 	@Override
 	public List<TDemo> selectByCondition(DemoCondition condition) {
-		TDemo demo = new TDemo();
-		demo.setName("栗子");
-		return demoMapper.selectList(new QueryWrapper<>(demo));
+		PageCondition pageCondition = condition.getPageCondition();
+		List<SortCondition> sortConditionList = condition.getSortConditionList();
+		QueryWrapper<TDemo> queryWrapper = new QueryWrapper<>();
+		queryWrapper.lambda().like(TDemo::getName, condition.getName()).lt(TDemo::getCreateTime, new Date()).eq(TDemo::getValid, Constants.COMMON_STATUS.VALID.getCode());
+		return demoMapper.selectList(queryWrapper);
 	}
 	@Override
 	public Integer addDemo(TDemo demo) {
--- springboot-mybatis-plus/src/test/java/com/liziczh/springboot/mybatisplus/MybatisPlusTest.java
+++ springboot-mybatis-plus/src/test/java/com/liziczh/springboot/mybatisplus/MybatisPlusTest.java
@@ -1,18 +1,69 @@
 package com.liziczh.springboot.mybatisplus;
 
-import org.junit.jupiter.api.Test;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.liziczh.base.common.util.JacksonUtils;
+import com.liziczh.springboot.mybatisplus.condition.DemoCondition;
+import com.liziczh.springboot.mybatisplus.constant.Constants;
+import com.liziczh.springboot.mybatisplus.entity.TDemo;
+import com.liziczh.springboot.mybatisplus.mapper.TDemoMapper;
+
 @RunWith(SpringRunner.class)
 @SpringJUnitConfig(classes = MybatisPlusApplication.class)
 @SpringBootTest
 @ActiveProfiles("test")
-class MybatisPlusTest {
+public class MybatisPlusTest {
+	@Autowired
+	private TDemoMapper demoMapper;
+
 	@Test
-	void addTest() {
+	public void selectByConditionTest() {
+		DemoCondition condition = new DemoCondition();
+		QueryWrapper<TDemo> queryWrapper = new QueryWrapper<>();
+		queryWrapper.lambda().like(TDemo::getName, "");
+		List<TDemo> demoList = demoMapper.selectList(queryWrapper);
+		System.out.println(JacksonUtils.toJSONString(demoList));
+	}
+	@Test
+	public void getAllTest() {
+		QueryWrapper<TDemo> queryWrapper = new QueryWrapper<>();
+		List<TDemo> demoList = demoMapper.selectList(queryWrapper);
+		System.out.println(JacksonUtils.toJSONString(demoList));
+	}
+	@Test
+	public void insertDemoTest() {
+		TDemo entity = new TDemo();
+		entity.setName("liziczh");
+		entity.setCreateTime(new Date());
+		entity.setCreateUser("lizi");
+		entity.setValid(Constants.COMMON_STATUS.VALID.getCode());
+		demoMapper.insert(entity);
+	}
+	@Test
+	public void updateDemoTest() {
+		TDemo entity = new TDemo();
+		entity.setId(24);
+		entity.setName("liziczh");
+		entity.setUpdateTime(new Date());
+		entity.setUpdateUser("lizi");
+		demoMapper.updateById(entity);
+	}
+	@Test
+	public void getDemoTest() {
+		demoMapper.selectById(24);
+	}
+	@Test
+	public void delDemoTest() {
+		demoMapper.deleteById(1);
 	}
 }
