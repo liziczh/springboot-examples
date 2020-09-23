5505ddbb25c863b68809634bb105ac337d520177
liziczh <liziczh@foxmail.com>
2020-09-23 10:53:46 +0800

    mp

--- springboot-mybatis-plus/src/main/java/com/liziczh/springboot/mybatisplus/mapper/TDemoMapper.java
+++ springboot-mybatis-plus/src/main/java/com/liziczh/springboot/mybatisplus/mapper/TDemoMapper.java
@@ -1,10 +1,12 @@
 package com.liziczh.springboot.mybatisplus.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.liziczh.springboot.mybatisplus.entity.TDemo;
 
+@Repository
 @Mapper
 public interface TDemoMapper extends BaseMapper<TDemo> {
 }
