0663a7439cb02c99075badba5b1fdd089a60f80a
liziczh <liziczh@foxmail.com>
2020-09-25 17:48:38 +0800

    kafka

--- springboot-kafka/src/main/java/com/liziczh/springboot/kafka/kafka/consumer/ConsumerListener.java
+++ springboot-kafka/src/main/java/com/liziczh/springboot/kafka/kafka/consumer/ConsumerListener.java
@@ -3,10 +3,13 @@ package com.liziczh.springboot.kafka.kafka.consumer;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 public class ConsumerListener {
 	@KafkaListener(topics = "archetypeTopic")
 	public void process(String msg) {
-		System.out.println(msg);
+		log.info(msg);
 	}
 }
